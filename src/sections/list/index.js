a843777a43fed8385082b27b8a796d8c33b45edc
huydq <huy.dq@tinasoft.com.vn>
2024-05-25 17:29:03 +0700

    update list resume

--- src/sections/list/index.js
+++ src/sections/list/index.js
@@ -1,7 +1,7 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 // @mui
-import { Box, Card } from '@mui/material'
+import { Box, Card, Grid } from '@mui/material'
 
 // components
 import BasicTable from 'components/BasicTable'
@@ -13,13 +13,34 @@ import useTable from 'hooks/useTable'
 
 import UserTableRow from './UserTableRow'
 import { TABLE_DESKTOP_HEAD, mockData } from './config'
+import { useSnackbar } from 'notistack'
+import { _getApi } from '../../utils/axios'
+import { fDateEndOfMonth, fDateStartOfMonth } from '../../utils/formatTime'
+import { AI_MODEL_OPTIONS } from '../chart/config'
+import RHFDatePicker from '../../components/form/RHFDatePicker'
+import FormProvider from '../../components/form/FormProvider'
+import { useForm } from 'react-hook-form'
+import { LoadingButton } from '@mui/lab'
 
 export default function ListUser() {
   const { page, setPage, rowsPerPage, onChangePage } = useTable({
-    defaultRowsPerPage: 10,
+    defaultRowsPerPage: 6,
+  })
+  const methods = useForm({
+    defaultValues: {
+      fromDate: fDateStartOfMonth(new Date()),
+      toDate: fDateEndOfMonth(new Date())
+    },
   })
 
-  const list = [...mockData]
+  const [listResume, setListResume] = useState([])
+  const [totalRecords, setTotalRecords] = useState(0);
+  const { enqueueSnackbar } = useSnackbar()
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods
 
   const columns = TABLE_DESKTOP_HEAD
 
@@ -27,24 +48,93 @@ export default function ListUser() {
     return <UserTableRow key={row?.id || index} row={row} />
   }, [])
 
+  const fetchData = useCallback(
+    async ({ params }) => {
+      try {
+        const response = await _getApi('resume', { params })
+
+        const data = response?.data?.map((item) => {
+          return {
+            ...item,
+            cvInJson: {
+              ...item?.cvInJson,
+              createdAt: item?.createdAt
+            }
+          }
+        })
+
+        setListResume(data)
+        setTotalRecords(response?.totalRecords)
+      } catch (error) {
+        enqueueSnackbar(error?.message, { variant: 'error' })
+      }
+    },
+    [enqueueSnackbar]
+  )
+
   useEffect(() => {
     setPage(DEFAULT_PAGE)
   }, [setPage])
 
+  useEffect(() => {
+    fetchData({
+      params: {
+        page_index: page,
+        page_size: rowsPerPage,
+        fromDate: fDateStartOfMonth(new Date())?.toISOString(),
+        toDate: fDateEndOfMonth(new Date())?.toISOString()
+      }
+    })
+  }, [fetchData, page])
+
+  const onSubmit = async (data) => {
+    const params = {
+      page_index: page,
+      page_size: rowsPerPage,
+      fromDate: data?.fromDate?.toISOString(),
+      toDate: data?.toDate?.toISOString(),
+    }
+    fetchData({ params })
+  }
+
   return (
     <Card>
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={1} fullWidth>
+          <Grid item xs={3}>
+            <RHFDatePicker label='From Date' name='fromDate' />
+          </Grid>
+
+          <Grid item xs={3}>
+            <RHFDatePicker label='To Date' name='toDate' />
+          </Grid>
+
+          <Grid item xs={2}>
+            <LoadingButton
+              type='submit'
+              variant='contained'
+              loading={isSubmitting}
+              size='large'
+              sx={{ height: '100%', width: '100%' }}
+            >
+              Search
+            </LoadingButton>
+          </Grid>
+        </Grid>
+      </FormProvider>
+
       <Box mt={2}>
         <BasicTable
           columns={columns}
           page={page}
           rowsPerPage={rowsPerPage}
-          dataSource={list}
+          dataSource={listResume}
           TableRowComp={tableRowComp}
           heightEmptyRow={0}
         />
 
         <Pagination
-          totalRecord={12}
+          totalRecord={totalRecords}
           page={page}
           rowsPerPage={rowsPerPage}
           onChangePage={onChangePage}
