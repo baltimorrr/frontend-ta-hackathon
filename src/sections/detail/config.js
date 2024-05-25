dcb2320abe349e0913620f0aa8aabc142f15d50e
huydq <huy.dq@tinasoft.com.vn>
2024-05-25 19:13:03 +0700

    update detail page

--- src/sections/detail/config.js
+++ src/sections/detail/config.js
@@ -5,3 +5,18 @@ export const TABLE_WORK_EXPERIENCE_HEAD = [
   { id: 'work_from', label: 'Work from', align: 'left' },
   { id: 'work_till', label: 'Work till', align: 'left' },
 ]
+
+export const TABLE_EDUCATION_HEAD = [
+  { id: 'university', label: 'University', align: 'left' },
+  { id: 'major', label: 'Major', align: 'left' },
+  { id: 'degree', label: 'Degree', align: 'left' },
+  { id: 'study_from', label: 'Study from', align: 'left' },
+  { id: 'study_till', label: 'Study till', align: 'left' },
+]
+
+export const TABLE_CERTIFICATIONS_HEAD = [
+  { id: 'name', label: 'Name', align: 'left' },
+  { id: 'issuing_organization', label: 'Issuing organization', align: 'left' },
+  { id: 'issue_date', label: 'Issue date', align: 'left' },
+  { id: 'expired_date', label: 'Expired date', align: 'left' },
+]
--- src/sections/detail/index.js
+++ src/sections/detail/index.js
@@ -1,15 +1,16 @@
 import { useCallback, useEffect, useState } from 'react'
 
 // @mui
-import { Box, Card, Typography } from '@mui/material'
+import { Box, Card, List, ListItemText, Typography } from '@mui/material'
 
 // components
 import BasicTable from 'components/BasicTable'
 import { useSnackbar } from 'notistack'
 import { _getApi } from '../../utils/axios'
-import { TABLE_WORK_EXPERIENCE_HEAD } from './config'
+import { TABLE_CERTIFICATIONS_HEAD, TABLE_EDUCATION_HEAD, TABLE_WORK_EXPERIENCE_HEAD } from './config'
 import WorkExperienceTableRow from './WorkExperienceTableRow'
-import HeaderBreadcrumbs from '../../components/HeaderBreadcrumbs'
+import EducationTableRow from './EducationTableRow'
+import CertificationsTableRow from './CertificationsTableRow'
 
 export default function DetailResume({ id }) {
   const [resumeDetail, setResumeDetail] = useState(null)
@@ -19,6 +20,18 @@ export default function DetailResume({ id }) {
 
   const tableWorkExperienceRowComp = useCallback((row, index) => {
     return <WorkExperienceTableRow key={row?.id || index} row={row} />
+  }, [])
+
+  const educationColumns = TABLE_EDUCATION_HEAD
+
+  const tableEducationRowComp = useCallback((row, index) => {
+    return <EducationTableRow key={row?.id || index} row={row} />
+  }, [])
+
+  const certificationsColumns = TABLE_CERTIFICATIONS_HEAD
+
+  const tableCertificationsRowComp = useCallback((row, index) => {
+    return <CertificationsTableRow key={row?.id || index} row={row} />
   }, [])
 
   const fetchData = useCallback(
@@ -90,7 +103,7 @@ export default function DetailResume({ id }) {
         resumeDetail?.cvInJson?.work_experience?.length > 0 && (
           <Card>
             <Box mt={2}>
-              <Typography variant='h5'>
+              <Typography variant='h5' ml={2}>
                 Work Experience
               </Typography>
 
@@ -108,19 +121,19 @@ export default function DetailResume({ id }) {
       }
 
       {
-        resumeDetail?.cvInJson?.work_experience?.length > 0 && (
+        resumeDetail?.cvInJson?.education?.length > 0 && (
           <Card>
             <Box mt={2}>
-              <Typography variant='h5'>
-                Work Experience
+              <Typography variant='h5' ml={2}>
+                Education
               </Typography>
 
               <BasicTable
-                columns={workExperienceColumns}
+                columns={educationColumns}
                 page={1}
-                rowsPerPage={resumeDetail?.cvInJson?.work_experience?.length}
-                dataSource={resumeDetail?.cvInJson?.work_experience}
-                TableRowComp={tableWorkExperienceRowComp}
+                rowsPerPage={resumeDetail?.cvInJson?.education?.length}
+                dataSource={resumeDetail?.cvInJson?.education}
+                TableRowComp={tableEducationRowComp}
                 heightEmptyRow={0}
               />
             </Box>
@@ -129,19 +142,46 @@ export default function DetailResume({ id }) {
       }
 
       {
-        resumeDetail?.cvInJson?.work_experience?.length > 0 && (
+        resumeDetail?.cvInJson?.skills?.length > 0 && (
+          <Card pl={2}>
+            <Box mt={2}>
+              <Typography variant='h5' ml={2}>
+                Skills
+              </Typography>
+
+              <List>
+                {
+                  resumeDetail?.cvInJson?.skills?.map((item) => {
+                    return (
+                      <>
+                        <ListItemText
+                          ml={2}
+                          primary={item}
+                        />
+                      </>
+                    )
+                  })
+                }
+              </List>
+            </Box>
+          </Card>
+        )
+      }
+
+      {
+        resumeDetail?.cvInJson?.certifications?.length > 0 && (
           <Card>
             <Box mt={2}>
-              <Typography variant='h5'>
-                Work Experience
+              <Typography variant='h5' ml={2}>
+                Certifications
               </Typography>
 
               <BasicTable
-                columns={workExperienceColumns}
+                columns={certificationsColumns}
                 page={1}
-                rowsPerPage={resumeDetail?.cvInJson?.work_experience?.length}
-                dataSource={resumeDetail?.cvInJson?.work_experience}
-                TableRowComp={tableWorkExperienceRowComp}
+                rowsPerPage={resumeDetail?.cvInJson?.certifications?.length}
+                dataSource={resumeDetail?.cvInJson?.certifications}
+                TableRowComp={tableCertificationsRowComp}
                 heightEmptyRow={0}
               />
             </Box>
